dcaef6ff3b970c9b5a6c420b12bc683f7d9d43e2
soundtomb <rhys.l.apfelbaum@gmail.com>
2025-02-11 16:34:26 +0000

    Add new config system

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+config_path = os.path.join(
+    os.path.dirname(__file__),
+    'config.json'
+)
+with open(config_path, 'r') as config_file:
+    config = json.load(config_file)
